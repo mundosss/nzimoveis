513b2d4e316b3bdcacea7a7e254f6cd8ed98d734
Leandro <mundosss@gmail.com>
2012-07-23 12:02:12 -0300

    database.yml changeds

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Nzimoveis::Application.configure do
   config.active_support.deprecation = :notify
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :address => 'webmail.nzimoveis.com.br/',          # mail.example.com
+      :address => 'webmail.nzimoveis.com.br',          # mail.example.com
       :domain => 'nzimoveis.com.br',                 # example.com
       :user_name => 'neuza@nzimoveis.com.br',            # user.name
       :password => 'neuza159*', # p@ssw0rd
