4b0d206d5480d94c0ab9144bd11bd65179617588
Leandro <mundosss@gmail.com>
2012-07-23 10:54:12 -0300

    contato

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,22 @@ Nzimoveis::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  # configuração de email
+  config.action_mailer.default_url_options = { :host => 'nzimoveis.com.br' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+      :address                 => "smtp.gmail.com",
+      :port                    =>  587,
+      :domain                  =>  "nzimoveis.com.br",
+      :user_name               =>  'mundosss@gmail.com',
+      :password                =>  'furiacd25',
+      :authentication          =>  'plain',
+      :enable_starttls_auto    =>   true
+  }
+
   # Enable threaded mode
   # config.threadsafe!
 
