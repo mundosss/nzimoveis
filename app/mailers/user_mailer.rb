612b4ea61e57a91f3356d45819763a7f38cce2b6
Leandro <mundosss@gmail.com>
2012-07-24 10:54:58 -0300

    nzimoveis

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class UserMailer < ActionMailer::Base
-  default from: "promocaovounessa@tecsis.inf.br"
+  default from: "mundosss@gmail.com"
 
 
   def mensagem_boas_vindas(user)
